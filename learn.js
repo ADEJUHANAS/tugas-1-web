2ffbdc5f947a2870b3374323519ec0d2030bc136
ADEJUHANAS <adejuhanas@studenr.uir.ac.id>
2022-01-02 19:43:57 +0700

    Latihan 5.8: Anomali pada javascript

--- learn.js
+++ learn.js
@@ -145,3 +145,14 @@ console.log(person["alamat"]);
 console.log(person["status"]);
 console.log(person["fullName"]());
 
+
+console.log("\nanomali pada javasript");
+console.log(0.1 + 0.2 == 0.3)
+console.log(1 + "1")
+console.log(1 - "1")
+console.log(1 < 2 < 3)
+console.log(3 > 2 > 1)
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0)
+
